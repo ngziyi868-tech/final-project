651b89afac33e786bdb02fe3e0cfb72718d7513d
ngziyi868-tech <ngziyi868@gmail.com>
2026-08-06 14:12:13 -0600

    using firebase instead

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDb success")
   } catch (error) {
-    console.error(error.messsage);
-    process.exit(1);
-  }
+  console.error("❌ MongoDB failed:");
+  console.error(error.message);
+  process.exit(1);
+}
 };
 
 export default connectDB;
